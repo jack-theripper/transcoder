b219b400ab36143933362af9e5f2f735424e26e3
Arhitector <lovesosabitch@yandex.ru>
2017-01-15 21:57:16 +0300

    Добавлено исключение ExecutableNotFoundException

---
+++ src/Exception/ExecutableNotFoundException.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * This file is part of the arhitector/jumper library.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author    Dmitry Arhitector <dmitry.arhitector@yandex.ru>
+ *
+ * @license   http://opensource.org/licenses/MIT MIT
+ * @copyright Copyright (c) 2017 Dmitry Arhitector <dmitry.arhitector@yandex.ru>
+ */
+namespace Arhitector\Jumper\Exception;
+
+use Exception;
+
+/**
+ * Executable file not found.
+ *
+ * @package Arhitector\Jumper\Exception
+ */
+class ExecutableNotFoundException extends TranscoderException
+{
+	
+	/**
+	 * @var string Executable file.
+	 */
+	protected $executable;
+	
+	/**
+	 * Construct the exception.
+	 *
+	 * @param string    $message  The Exception message to throw.
+	 * @param string    $executableFile
+	 * @param Exception $previous The previous exception used for the exception chaining.
+	 */
+	public function __construct($message, $executableFile, Exception $previous = null)
+	{
+		$this->setExecutableFile($executableFile);
+		
+		parent::__construct($message, 0, $previous);
+	}
+	
+	/**
+	 * Get binary value.
+	 *
+	 * @return string
+	 */
+	public function getExecutableFile()
+	{
+		return $this->executable;
+	}
+	
+	/**
+	 * Sets the binary value.
+	 *
+	 * @param string $executableFile
+	 *
+	 * @return ExecutableNotFoundException
+	 */
+	protected function setExecutableFile($executableFile)
+	{
+		$this->executable = (string) $executableFile;
+		
+		return $this;
+	}
+	
+}
