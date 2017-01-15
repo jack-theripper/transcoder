97a81924b3e84ca3ba3fd9d92729260e49db58f8
Arhitector <lovesosabitch@yandex.ru>
2017-01-15 21:56:19 +0300

    Добавлено исключение TranscoderException

---
+++ src/Exception/TranscoderException.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is part of the arhitector/jumper library.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author    Dmitry Arhitector <dmitry.arhitector@yandex.ru>
+ *
+ * @license   http://opensource.org/licenses/MIT MIT
+ * @copyright Copyright (c) 2016 Dmitry Arhitector <dmitry.arhitector@yandex.ru>
+ */
+namespace Arhitector\Jumper\Exception;
+
+/**
+ * An exception during transcoding.
+ *
+ * @package Arhitector\Jumper\Exception
+ */
+class TranscoderException extends \RuntimeException
+{
+	
+}
