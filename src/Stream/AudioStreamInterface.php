25c69ddd25dbd78b9ba1633024101036234f17f6
Arhitector <lovesosabitch@yandex.ru>
2017-01-15 23:48:36 +0300

    Добавлен AudioStreamInterface

---
+++ src/Stream/AudioStreamInterface.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * This file is part of the arhitector/jumper library.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author    Dmitry Arhitector <dmitry.arhitector@yandex.ru>
+ *
+ * @license   http://opensource.org/licenses/MIT MIT
+ * @copyright Copyright (c) 2017 Dmitry Arhitector <dmitry.arhitector@yandex.ru>
+ */
+namespace Arhitector\Jumper\Stream;
+
+/**
+ * Interface AudioStreamInterface.
+ *
+ * @package Arhitector\Jumper\Stream
+ */
+interface AudioStreamInterface extends StreamInterface
+{
+	
+	/**
+	 * Get channels value.
+	 *
+	 * @return int
+	 */
+	public function getChannels();
+	
+	/**
+	 * Get sample rate value.
+	 *
+	 * @return string
+	 */
+	public function getFrequency();
+	
+}
