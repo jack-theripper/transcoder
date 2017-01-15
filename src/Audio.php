aa7b5e429382df1d301c24d7f194d537d5ba8c01
Arhitector <lovesosabitch@yandex.ru>
2017-01-15 23:30:19 +0300

    Добавлен AudioFormat

--- src/Audio.php
+++ src/Audio.php
@@ -67,12 +67,115 @@ class Audio implements AudioInterface
 		$this->setFilePath($filePath);
 		$this->decoder = new Decoder($options);
 		
+		/** @noinspection ExceptionsAnnotatingAndHandlingInspection */
 		$demuxing = $this->decoder->demuxing($this);
 		
 		if (count($demuxing->streams) < 1 || ( ! $this->isSupportedFileType() && empty($demuxing->format['format'])))
 		{
 			throw new TranscoderException('File type unsupported or the file is corrupted.');
 		}
+		
+		
+		
+		/* class stdClass#4 (2) {
+  public $format =>
+  array(12) {
+    'filename' =>
+    string(68) "E:\OpenServer_525\OpenServer\domains\localhost\git\jumper\audio1.mp3"
+    'nb_streams' =>
+    int(1)
+    'nb_programs' =>
+    int(0)
+    'format_name' =>
+    string(3) "mp3"
+    'format_long_name' =>
+    string(28) "MP2/3 (MPEG audio layer 2/3)"
+    'start_time' =>
+    string(8) "0.000000"
+    'duration' =>
+    string(10) "194.063675"
+    'size' =>
+    string(7) "7762599"
+    'bit_rate' =>
+    string(6) "320002"
+    'probe_score' =>
+    int(51)
+    'tags' =>
+    array(3) {
+      'TBPM' =>
+      string(1) "0"
+      'encoded_by' =>
+      string(4) "LAME"
+      'date' =>
+      string(4) "2015"
+    }
+    'format' =>
+    string(3) "mp3"
+  }
+  public $streams =>
+  array(1) {
+    [0] =>
+    array(24) {
+      'frequency' =>
+      int(44100)
+      'channels' =>
+      int(2)
+      'index' =>
+      int(0)
+      'type' =>
+      string(5) "audio"
+      'profile' =>
+      string(0) ""
+      'bit_rate' =>
+      string(6) "320000"
+      'start_time' =>
+      string(8) "0.000000"
+      'duration' =>
+      string(10) "194.063675"
+      'properties' =>
+      array(0) {
+      }
+      'codec_name' =>
+      string(3) "mp3"
+      'codec_long_name' =>
+      string(24) "MP3 (MPEG audio layer 3)"
+      'codec_type' =>
+      string(5) "audio"
+      'codec_time_base' =>
+      string(7) "1/44100"
+      'codec_tag_string' =>
+      string(12) "[0][0][0][0]"
+      'codec_tag' =>
+      string(6) "0x0000"
+      'sample_fmt' =>
+      string(4) "s16p"
+      'channel_layout' =>
+      string(6) "stereo"
+      'bits_per_sample' =>
+      int(0)
+      'r_frame_rate' =>
+      string(3) "0/0"
+      'avg_frame_rate' =>
+      string(3) "0/0"
+      'time_base' =>
+      string(10) "1/14112000"
+      'start_pts' =>
+      int(0)
+      'duration_ts' =>
+      double(2738626582)
+      'codec' =>
+      class Arhitector\Jumper\Codec#6 (2) {
+        protected $codec =>
+        string(3) "mp3"
+        protected $name =>
+        string(24) "MP3 (MPEG audio layer 3)"
+      }
+    }
+  }
+ */
+		
+		var_dump($demuxing);
+		
 	}
 	
 	/**
