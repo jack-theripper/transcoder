f4bb97e26724797d5ddcfd0c336ebefc4838a609
Arhitector <lovesosabitch@yandex.ru>
2017-01-20 20:28:16 +0300

    Добавлен объект формата Mp3

---
+++ src/Format/Mp3.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * This file is part of the arhitector/jumper library.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author    Dmitry Arhitector <dmitry.arhitector@yandex.ru>
+ *
+ * @license   http://opensource.org/licenses/MIT MIT
+ * @copyright Copyright (c) 2017 Dmitry Arhitector <dmitry.arhitector@yandex.ru>
+ */
+namespace Arhitector\Jumper\Format;
+
+use Arhitector\Jumper\Codec;
+
+/**
+ * Class Mp3.
+ *
+ * @package Arhitector\Jumper\Format
+ */
+class Mp3 extends AudioFormat
+{
+	
+	/**
+	 * @var array List of extensions.
+	 */
+	protected $extensions = ['mp2', 'mp3', 'm2a'];
+	
+	/**
+	 * AudioFormat constructor.
+	 *
+	 * @param Codec|string $audioCodec
+	 *
+	 * @throws \InvalidArgumentException
+	 */
+	public function __construct($audioCodec = 'libmp3lame')
+	{
+		$this->setAvailableAudioCodecs(['libmp3lame', 'libshine', 'mp3', 'mp3pro', 'lame']);
+		
+		parent::__construct($audioCodec);
+	}
+	
+}
