2af681f89f3f65059f13bbdf3663e81d2bc762dc
Arhitector <lovesosabitch@yandex.ru>
2017-01-21 23:22:20 +0300

    SimpleFilter реализует ArrayAccess

--- src/Filter/SimpleFilter.php
+++ src/Filter/SimpleFilter.php
@@ -20,7 +20,7 @@ use Arhitector\Jumper\TranscoderInterface;
  *
  * @package Arhitector\Jumper\Filter
  */
-class SimpleFilter implements AudioFilterInterface
+class SimpleFilter implements AudioFilterInterface, \ArrayAccess
 {
 	
 	/**
@@ -52,7 +52,6 @@ class SimpleFilter implements AudioFilterInterface
 		return $this;
 	}
 	
-	
 	/**
 	 * Apply filter.
 	 *
@@ -74,6 +73,69 @@ class SimpleFilter implements AudioFilterInterface
 	public function toArray()
 	{
 		return $this->parameters;
+	}
+	
+	/**
+	 * Whether a offset exists
+	 *
+	 * @param mixed $offset
+	 *
+	 * @return boolean
+	 */
+	public function offsetExists($offset)
+	{
+		return array_key_exists($offset, $this->parameters);
+	}
+	
+	/**
+	 * Offset to retrieve
+	 *
+	 * @param mixed $offset
+	 *
+	 * @return mixed
+	 * @throws \OutOfBoundsException
+	 */
+	public function offsetGet($offset)
+	{
+		if ( ! $this->offsetExists($offset))
+		{
+			throw new \OutOfBoundsException('Index invalid or out of range.');
+		}
+		
+		return $this->parameters[$offset];
+	}
+	
+	/**
+	 * Offset to set
+	 *
+	 * @param mixed $offset The offset to assign the value to.
+	 * @param mixed $value  The value to set.
+	 *
+	 * @return SimpleFilter
+	 * @throws \InvalidArgumentException
+	 */
+	public function offsetSet($offset, $value)
+	{
+		if ( ! is_scalar($offset))
+		{
+			throw new \InvalidArgumentException('The offset value must be a scalar.');
+		}
+		
+		return $this;
+	}
+	
+	/**
+	 * Remove parameter.
+	 *
+	 * @param mixed $parameter
+	 *
+	 * @return SimpleFilter
+	 */
+	public function offsetUnset($parameter)
+	{
+		unset($this->parameters[$parameter]);
+		
+		return $this;
 	}
 	
 }
