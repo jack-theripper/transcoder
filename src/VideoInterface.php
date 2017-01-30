de5c1551625980daa3c5f5350f7a88c6ace7bda5
Arhitector <lovesosabitch@yandex.ru>
2017-01-30 23:33:55 +0300

    Добавлен VideoInterface

---
+++ src/VideoInterface.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * This file is part of the arhitector/jumper library.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author    Dmitry Arhitector <dmitry.arhitector@yandex.ru>
+ *
+ * @license   http://opensource.org/licenses/MIT MIT
+ * @copyright Copyright (c) 2017 Dmitry Arhitector <dmitry.arhitector@yandex.ru>
+ */
+namespace Arhitector\Jumper;
+
+/**
+ * Interface VideoInterface.
+ *
+ * @package Arhitector\Jumper
+ */
+interface VideoInterface extends AudioInterface
+{
+	
+	/**
+	 * Returns the video codec.
+	 *
+	 * @return Codec|null
+	 */
+	public function getVideoCodec();
+	
+	/**
+	 * Get width value.
+	 *
+	 * @return int
+	 */
+	public function getWidth();
+	
+	/**
+	 * Get height value.
+	 *
+	 * @return int
+	 */
+	public function getHeight();
+	
+	/**
+	 * Gets the bitrate value.
+	 *
+	 * @return int
+	 */
+	public function getKiloBitrate();
+	
+	/**
+	 * Get frame rate value.
+	 *
+	 * @return float
+	 */
+	public function getFrameRate();
+	
+}
