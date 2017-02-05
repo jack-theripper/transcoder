f6fe7c47d2f39cbaa1ac6097597102b8634bfc5f
Arhitector <lovesosabitch@yandex.ru>
2017-02-05 17:17:24 +0300

    Добавлен TranscoderTrait

---
+++ src/TranscoderTrait.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * This file is part of the arhitector/jumper library.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author    Dmitry Arhitector <dmitry.arhitector@yandex.ru>
+ *
+ * @license   http://opensource.org/licenses/MIT MIT
+ * @copyright Copyright (c) 2017 Dmitry Arhitector <dmitry.arhitector@yandex.ru>
+ */
+namespace Arhitector\Jumper;
+
+use Arhitector\Jumper\Format\FormatInterface;
+use Arhitector\Jumper\Stream\StreamInterface;
+
+/**
+ * Class TranscoderTrait.
+ *
+ * @package Arhitector\Jumper
+ * @mixin TranscoderInterface
+ */
+trait TranscoderTrait
+{
+	
+	/**
+	 * @var FormatInterface
+	 */
+	protected $format;
+	
+	/**
+	 * @var \Arhitector\Jumper\Stream\Collection
+	 */
+	protected $streams;
+	
+	/**
+	 * Get current format.
+	 *
+	 * @return FormatInterface
+	 */
+	public function getFormat()
+	{
+		return $this->format;
+	}
+	
+	/**
+	 * Get a list of streams.
+	 *
+	 * @return \Arhitector\Jumper\Stream\Collection|StreamInterface[]
+	 */
+	public function getStreams()
+	{
+		return $this->streams;
+	}
+	
+}
