108cabf9c16513d970b901affb132fd8fa604bf2
Arhitector <lovesosabitch@yandex.ru>
2017-02-07 19:32:01 +0300

    Exceptions обновлены

--- src/Exception/ExecutableNotFoundException.php
+++ src/Exception/ExecutableNotFoundException.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This file is part of the arhitector/jumper library.
+ * This file is part of the arhitector/transcoder library.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -10,14 +10,14 @@
  * @license   http://opensource.org/licenses/MIT MIT
  * @copyright Copyright (c) 2017 Dmitry Arhitector <dmitry.arhitector@yandex.ru>
  */
-namespace Arhitector\Jumper\Exception;
+namespace Arhitector\Transcoder\Exception;
 
 use Exception;
 
 /**
  * Executable file not found.
  *
- * @package Arhitector\Jumper\Exception
+ * @package Arhitector\Transcoder\Exception
  */
 class ExecutableNotFoundException extends TranscoderException
 {
