8668d52a7dd7be9e2b8c278c8b378df0a744bb4e
Arhitector <lovesosabitch@yandex.ru>
2017-03-29 12:25:33 +0300

    Добавлена обертка над Jpeg

---
+++ src/Format/Jpeg.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file is part of the arhitector/transcoder library.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author    Dmitry Arhitector <dmitry.arhitector@yandex.ru>
+ *
+ * @license   http://opensource.org/licenses/MIT MIT
+ * @copyright Copyright (c) 2017 Dmitry Arhitector <dmitry.arhitector@yandex.ru>
+ */
+namespace Arhitector\Transcoder\Format;
+
+use Arhitector\Transcoder\Codec;
+
+/**
+ * The Jpeg picture format.
+ *
+ * @package Arhitector\Transcoder\Format
+ */
+class Jpeg extends FrameFormat
+{
+	
+	/**
+	 * Format constructor.
+	 *
+	 * @param Codec|string $codec
+	 *
+	 * @throws \InvalidArgumentException
+	 */
+	public function __construct($codec = 'mjpeg')
+	{
+		parent::__construct($codec);
+		
+		$this->setExtensions(['jpg', 'jpeg']);
+		$this->setAvailableVideoCodecs(['jpeg2000', 'libopenjpeg', 'jpegls', 'ljpeg', 'mjpeg']);
+	}
+	
+}
