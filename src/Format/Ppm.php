c3131882460a325ef07cce0316145a39d40ac44f
Arhitector <lovesosabitch@yandex.ru>
2017-03-29 12:29:18 +0300

    Добавлена обёртка над Ppm

---
+++ src/Format/Ppm.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file is part of the arhitector/transcoder library.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author    Dmitry Arhitector <dmitry.arhitector@yandex.ru>
+ *
+ * @license   http://opensource.org/licenses/MIT MIT
+ * @copyright Copyright (c) 2017 Dmitry Arhitector <dmitry.arhitector@yandex.ru>
+ */
+namespace Arhitector\Transcoder\Format;
+
+use Arhitector\Transcoder\Codec;
+
+/**
+ * The Ppm picture format.
+ *
+ * @package Arhitector\Transcoder\Format
+ */
+class Ppm extends FrameFormat
+{
+	
+	/**
+	 * Format constructor.
+	 *
+	 * @param Codec|string $codec
+	 *
+	 * @throws \InvalidArgumentException
+	 */
+	public function __construct($codec = 'ppm')
+	{
+		parent::__construct($codec);
+		
+		$this->setExtensions(['ppm']);
+		$this->setAvailableVideoCodecs(['ppm']);
+	}
+	
+}
