5cebe9c1016e10c0a13256cd0e79ba2ff97af36a
Arhitector <lovesosabitch@yandex.ru>
2017-03-29 22:03:34 +0300

    Добавлена обертка для Aac

---
+++ src/Format/Aac.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file is part of the arhitector/transcoder library.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author    Dmitry Arhitector <dmitry.arhitector@yandex.ru>
+ *
+ * @license   http://opensource.org/licenses/MIT MIT
+ * @copyright Copyright (c) 2017 Dmitry Arhitector <dmitry.arhitector@yandex.ru>
+ */
+namespace Arhitector\Transcoder\Format;
+
+use Arhitector\Transcoder\Codec;
+
+/**
+ * The Aac audio format.
+ *
+ * @package Arhitector\Transcoder\Format
+ */
+class Aac extends AudioFormat
+{
+	
+	/**
+	 * Format constructor.
+	 *
+	 * @param Codec|string $audioCodec
+	 *
+	 * @throws \InvalidArgumentException
+	 */
+	public function __construct($audioCodec = 'aac')
+	{
+		parent::__construct($audioCodec, null);
+		
+		$this->setExtensions(['aac']);
+		$this->setAvailableVideoCodecs(['libfdk_aac', 'libfaac', 'aac', 'libvo_aacenc', 'faac']);
+	}
+	
+}
