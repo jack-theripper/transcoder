2aa0a875b632d2d7dba5f8f5747c51478a7d9d43
Arhitector <lovesosabitch@yandex.ru>
2017-03-30 10:58:44 +0300

    Добавлен EmitterInterface и EmitterTrait

---
+++ src/Event/EmitterInterface.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * This file is part of the arhitector/transcoder library.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author    Dmitry Arhitector <dmitry.arhitector@yandex.ru>
+ *
+ * @license   http://opensource.org/licenses/MIT MIT
+ * @copyright Copyright (c) 2017 Dmitry Arhitector <dmitry.arhitector@yandex.ru>
+ */
+namespace Arhitector\Transcoder\Event;
+
+use League\Event\EventInterface;
+use League\Event\ListenerAcceptorInterface;
+use League\Event\ListenerInterface;
+
+/**
+ * Interface EmitterInterface.
+ *
+ * @package Arhitector\Transcoder\Event
+ */
+interface EmitterInterface extends ListenerAcceptorInterface
+{
+	
+	/**
+	 * Emit an event.
+	 *
+	 * @param string|EventInterface $event
+	 *
+	 * @return EventInterface
+	 */
+	public function emit($event);
+	
+}
---
+++ src/Event/EmitterTrait.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This file is part of the arhitector/transcoder library.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author    Dmitry Arhitector <dmitry.arhitector@yandex.ru>
+ *
+ * @license   http://opensource.org/licenses/MIT MIT
+ * @copyright Copyright (c) 2017 Dmitry Arhitector <dmitry.arhitector@yandex.ru>
+ */
+namespace Arhitector\Transcoder\Event;
+
+use League\Event\EmitterTrait as EventEmitterTrait;
+
+/**
+ * Class EmitterTrait.
+ *
+ * @package Arhitector\Transcoder\Event
+ */
+trait EmitterTrait
+{
+	use EventEmitterTrait;
+	
+}
